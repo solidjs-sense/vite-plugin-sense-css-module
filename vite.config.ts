e14096d82bd665bfb3d80298093e851cb551b204
iamcco <ooiss@qq.com>
2022-09-16 12:31:53 +0800

    chore(build): add vite

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      tsConfigFilePath: 'tsconfig.json',
+      insertTypesEntry: true,
+      noEmitOnError: true,
+      skipDiagnostics: false,
+      logDiagnostics: true,
+    }),
+  ],
+  build: {
+    target: 'ES2016',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['cjs'],
+      fileName: () => 'index.cjs',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        'path',
+        'fs',
+        'fs/promises',
+        'typescript'
+      ],
+    },
+  },
+});
